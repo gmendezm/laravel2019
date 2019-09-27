742f0a8b1b0d4737c8327ffe93eb1089d0008b8b
gmendez-sa <giovanni.mendez@avantica.net>
2019-09-27 14:12:04 -0600

    Posts: Showing JSON list of all the results in /posts view.

--- posts_project/app/Http/Controllers/PostController.php
+++ posts_project/app/Http/Controllers/PostController.php
@@ -14,7 +14,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        return view('posts.index');
+       return Post::all();
+       return view('posts.index');
     }
 
     /**
