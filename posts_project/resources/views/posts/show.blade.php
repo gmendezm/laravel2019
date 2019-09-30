61f8e0eaa055bd2dabac490e3e662dd8f752975f
gmendez-sa <giovanni.mendez@avantica.net>
2019-09-30 13:17:17 -0600

    Posts: Added functionality to show formatted html code.

--- posts_project/resources/views/posts/show.blade.php
+++ posts_project/resources/views/posts/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <a href="/posts" class="btn btn-primary">Go Back</a>
     <div class="card p-3 m-3">
-        <h1>{{$post->title}}</h1>
+        <h1>{!!$post->title!!}</h1>
         <small>Written on {{ $post->created_at }}</small>
         <div>
             {{$post->body}}
