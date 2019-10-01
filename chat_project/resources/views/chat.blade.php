d4086c90b7438b3d01834ec1d96daa03179b459d
gmendez-sa <giovanni.mendez@avantica.net>
2019-10-01 16:07:15 -0600

    Chat: Added the third party component VueChatScroll for the auto scroll in the chat.

--- chat_project/resources/views/chat.blade.php
+++ chat_project/resources/views/chat.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="w-50 m-3 mx-auto" id="app">
         <div class="list-group-item active">Chat Room</div>
-        <ul class="list-group  chat-window" >
+        <ul class="list-group chat-window" v-chat-scroll>
             <message v-for="value in chat.message"> @{{ value }}</message>
         </ul>
         <input style="width:100%;" type="text" class="form-control p-3" placeholder="Write something" v-model='message' @keyup.enter="send">
