22b4920a651cde9c2af2457751edaead507ef752
gmendez-sa <giovanni.mendez@avantica.net>
2019-10-01 16:40:48 -0600

    Chat: Customized the way the images and a nick name will be shown.

--- chat_project/resources/js/components/chat/message.vue
+++ chat_project/resources/js/components/chat/message.vue
@@ -1,9 +1,23 @@
 <template>
-    <li class="list-group-item"><slot></slot></li>
+   <div class="m-2">
+        <li class="list-group-item" :class="className"><slot></slot></li>
+        <small class="badge float-right" :class='badgeClass'>Tu</small>
+   </div>
 </template>
 
 <script>
     export default {
+        props: [
+            'color'
+        ], 
+        computed: {
+            className(){
+                return 'list-group-item-'+ this.color;
+            },
+            badgeClass(){
+                return 'badge-'  +this.color;
+            }
+        },
         mounted(){
             console.log("Component mounted.");
         }
