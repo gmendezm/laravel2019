c068554b33e9584e5818a4957adecf4f69146a53
gmendez-sa <giovanni.mendez@avantica.net>
2019-10-01 14:45:59 -0600

    Chat: Some app configurations.

--- chat_project/routes/web.php
+++ chat_project/routes/web.php
@@ -12,9 +12,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('chat');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/chat', function () {
+    return view('chat');
+});
