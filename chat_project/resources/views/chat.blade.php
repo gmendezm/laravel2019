39e50a92f6428b6341f1772c7e318a2c7be28f2f
gmendez-sa <giovanni.mendez@avantica.net>
2019-10-01 15:06:19 -0600

    Chat: Implemented a comunication prototype pipe between the chat blade and the vue pipe code using v-model.

--- chat_project/resources/views/chat.blade.php
+++ chat_project/resources/views/chat.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="w-50 m-3 mx-auto">
+    <div class="w-50 m-3 mx-auto" id="app">
         <ul class="list-group" >
             <li class="list-group-item active">Chat Room</li>
             <li class="list-group-item">Message Example</li>
-            <input type="text" class="form-conrol p-3" placeholder="Write something">
+            <input type="text" class="form-conrol p-3" placeholder="Write something" v-model='message'>
         </ul>
     </div>
 @endsection
