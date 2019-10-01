8d7004f5f4ea3aa9d89c7b3222d4e37d278e8a95
gmendez-sa <giovanni.mendez@avantica.net>
2019-10-01 15:47:49 -0600

    Chat: Propagated the component to the view so now the messages are shown in the chat window.

--- chat_project/resources/views/chat.blade.php
+++ chat_project/resources/views/chat.blade.php
@@ -4,8 +4,8 @@
     <div class="w-50 m-3 mx-auto" id="app">
         <ul class="list-group" >
             <li class="list-group-item active">Chat Room</li>
-            <li class="list-group-item">Message Example</li>
-            <input type="text" class="form-conrol p-3" placeholder="Write something" v-model='message'>
+            <message v-for="value in chat.message"> @{{ value }}</message>
         </ul>
+        <input style="width:100%;" type="text" class="form-conrol p-3" placeholder="Write something" v-model='message' @keyup.enter="send">
     </div>
 @endsection
