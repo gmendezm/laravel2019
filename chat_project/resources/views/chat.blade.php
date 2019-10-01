f050a283f67c96933d599b07004616b5d29e1725
gmendez-sa <giovanni.mendez@avantica.net>
2019-10-01 14:46:30 -0600

    Chat: Added a template for the chat UI using boostrap classes.

---
+++ chat_project/resources/views/chat.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="w-50 m-3 mx-auto">
+        <ul class="list-group" >
+            <li class="list-group-item active">Chat Room</li>
+            <li class="list-group-item">Message Example</li>
+            <input type="text" class="form-conrol p-3" placeholder="Write something">
+        </ul>
+    </div>
+@endsection
