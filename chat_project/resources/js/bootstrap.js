6435d9bf385fa742b2e9b4afde98246f70d5b9e4
gmendez-sa <giovanni.mendez@avantica.net>
2019-10-02 09:42:08 -0600

    Chat: Configured the Laravel Echo code in the boostrap.js file.

--- chat_project/resources/js/bootstrap.js
+++ chat_project/resources/js/bootstrap.js
@@ -43,13 +43,15 @@ if (token) {
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from 'laravel-echo'
+ import Echo from 'laravel-echo'
 
-// window.Pusher = require('pusher-js');
+ window.Pusher = require('pusher-js');
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     encrypted: true
-// });
+ window.Echo = new Echo({
+     broadcaster: 'pusher',
+     //key: process.env.MIX_PUSHER_APP_KEY,
+     //cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+     key: 'd1295a63ebca875553be',
+     cluster: 'us2',
+     encrypted: true
+ });
