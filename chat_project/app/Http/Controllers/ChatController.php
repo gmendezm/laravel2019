266b56247374a3b8f4a05eda17f677585a80bd99
gmendez-sa <giovanni.mendez@avantica.net>
2019-10-02 09:23:47 -0600

    Chat: Added security so now the users must be logged in to acces the chat.

--- chat_project/app/Http/Controllers/ChatController.php
+++ chat_project/app/Http/Controllers/ChatController.php
@@ -3,10 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Auth;
 
 class ChatController extends Controller
 {
-    public function chat(){
-        
+
+   /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
     }
+
+    public function chat(){
+        return view('chat');
+    }
+
 }
